472fd9d47bacb8d28271c51230c9c6b62ec296c9
Jannik Weise <jannik.weise99@gmail.com>
2017-08-23 14:31:42 +0200

    Modified config, added counter.ini
    Added counter.ini to accomodate the Retweet Counter so the config won't get lost if theres an unexpected crash during writing / renaming of the file.
    
    Dropped Latest_ID.

--- twitter-retweet-bot.py
+++ twitter-retweet-bot.py
@@ -13,10 +13,13 @@ ACCESS_TOKEN_SECRET = lines[3]
 ACCOUNT_NAME = lines[4]
 LIST_NAME = lines[5]
 
-RETWEET_COUNTER = int(lines[6])
-LATEST_ID = lines[7]
-
 f.close()
+
+f1 = open("counter.ini", "r")
+
+RETWEET_COUNTER = int(f1.read())
+
+f1.close()
 
 try:
     twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -31,11 +34,7 @@ if initiated:
 
     while True:
         try:
-            ids = twitter.get_list_statuses(slug=LIST_NAME, owner_screen_name=ACCOUNT_NAME, include_rts=0, since_id=LATEST_ID)
-
-            if not len(ids) == 0:
-                LATEST_ID_SAVE = ids[0]["id"]
-                LATEST_ID = str(LATEST_ID_SAVE)
+            ids = twitter.get_list_statuses(slug=LIST_NAME, owner_screen_name=ACCOUNT_NAME, include_rts=0)
 
             for i in range(len(ids)):
                 if not ids[i]['retweeted']:
@@ -43,18 +42,11 @@ if initiated:
                     RETWEET_COUNTER += 1
                     print("Retweeted", "ID", ids[i]["id_str"], "Retweet Number:", RETWEET_COUNTER)
 
-            f1 = open("config_tmp", "w")
-            f1.write(CONSUMER_KEY + '\n')
-            f1.write(CONSUMER_SECRET + '\n')
-            f1.write(ACCESS_TOKEN + '\n')
-            f1.write(ACCESS_TOKEN_SECRET + '\n')
-            f1.write(ACCOUNT_NAME + '\n')
-            f1.write(LIST_NAME + '\n')
-            f1.write(str(RETWEET_COUNTER) + '\n')
-            f1.write(LATEST_ID + '\n')
+            f1 = open("counter_tmp", "w")
+            f1.write(str(RETWEET_COUNTER))
             f1.close()
-            os.remove("config.ini")
-            os.rename("config_tmp", "config.ini")
+            os.remove("counter.ini")
+            os.rename("counter_tmp", "counter.ini")
 
         except TwythonError as e:
             print(e.error_code)
